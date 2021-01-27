7a294865059ab7b7f390d5e659a1474a13159edf
Mataeux Gauthier <mataeuxg@gmail.com>
2021-01-26 16:02:46 -0800

    Build basic css layout of game board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  state = {
+    rows: 6,
+    columns: 7,
+    moves: [],
+    playerTurn: 'red',
+  };
+
+  resetBoard = () => {
+    this.setState({ moves: [] });
+  }
+
+  getPiece = (x, y) => {
+    const list = this.state.moves.filter((item) => {
+      return (item.x === x && item.y === y);
+    });
+    return list[0];
+  }
+
+
+
+  renderBoard() {
+    const { rows, columns } = this.state;
+    const rowViews = [];
+
+    for (let row = 0; row < this.state.rows; row++) {
+      const columnViews = [];
+      for (let column = 0; column < this.state.columns; column++) {
+        const piece = this.getPiece(column, row);
+        columnViews.push(
+          <div onClick={() => {this.addMove(column, row)}} style={{ backgroundColor: '#00a8ff', width: '8vw', height: '8vw', display: 'flex', padding: 5 , cursor: 'pointer' }}>
+            <div style={{ borderRadius: '50%', backgroundColor: 'white', flex: 1, display: 'flex' }}>
+              {piece ? <div style={{ flex: 1, borderRadius: '50%', border: '1px solid #333', backgroundColor: piece.player }}/> : undefined}
+            </div>
+          </div>
+        );
+      }
+      rowViews.push(
+        <div style={{ display: 'flex', flexDirection: 'row' }}>{columnViews}</div>
+      );
+    }
+    return (
+      <div style={{backgroundColor: 'red', display: 'flex', flexDirection: 'column' }}>
+        {rowViews}
+      </div>
+    )
+  }
+
+
+  render() {
+    const { style } = this.props;
+
+    return (
+      <div style={style ? Object.assign({}, styles.container, style) : styles.container}>
+        <div>
+          {this.renderBoard()}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+const styles = {
+  container: {
+    height: '100%',
+    padding: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+};
